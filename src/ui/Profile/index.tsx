c28c8e11390c22dfbb8eb9009e0e5525eb201192
Solene <solene.cuffet@hotmail.fr>
2022-04-02 16:07:15 +0200

    Profile

--- src/ui/Profile/index.tsx
+++ src/ui/Profile/index.tsx
@@ -1,36 +1,30 @@
-import {Button, Container, Stack, TextField} from "@mui/material";
+import { Button, Container, Stack, TextField } from "@mui/material";
 import Camera from "../Shared/Camera";
-import {setYears} from "../../core/Store/yearsSlice";
-import {setWeeks} from "../../core/Store/weeksSlice";
-import {setPage} from "../../core/Store/appSlice";
-import {useContext, useEffect, useState} from "react";
-import SignInContext, {User} from "../../core/SignInContext";
-import {useNavigate} from "react-router-dom";
-import {useAppDispatch} from "../../core/Store";
+import { setYears } from "../../core/Store/yearsSlice";
+import { setWeeks } from "../../core/Store/weeksSlice";
+import { setPage } from "../../core/Store/appSlice";
+import { useContext, useEffect, useState } from "react";
+import SignInContext, { User } from "../../core/SignInContext";
+import { useNavigate } from "react-router-dom";
+import { useAppDispatch } from "../../core/Store";
 
 export default function Profile() {
 
     const navigate = useNavigate();
-    const context = useContext(SignInContext);
     const dispatch = useAppDispatch();
+
 
     useEffect(() => {
         // @ts-ignore
-        const user = JSON.parse(localStorage.getItem('user')) ;
+        const user = JSON.parse(localStorage.getItem('user'));
         if (user) {
-            setUser(user) ;
+            setUser(user);
         }
-        if (
-            new Date(context.user.birthdate).toLocaleDateString() !==
-            new Date("10/10/1910").toLocaleDateString()
-        ) {
-            navigate("/years");
-        }
-    }, [context, navigate]);
+
+    }, [navigate]);
 
 
     const handleProfil = () => {
-        context.setUser(user);
         dispatch(setYears({ birthdate: user.birthdate }));
         dispatch(setWeeks({ birthdate: user.birthdate }));
         dispatch(setPage("/years"));
