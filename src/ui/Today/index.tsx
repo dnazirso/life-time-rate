dfba67d40e3f673e6bc2067ac66eca6e5f90f6b1
laurabjn <92058813+laurabjn@users.noreply.github.com>
2022-04-05 15:53:32 +0200

    feat: 🎸 log happiness improve slider

--- src/ui/Today/index.tsx
+++ src/ui/Today/index.tsx
@@ -1,4 +1,4 @@
-import { Celebration, SentimentVeryDissatisfied } from "@mui/icons-material";
+import { SentimentVerySatisfied, MoodBad, Save } from '@mui/icons-material';
 import {
   Stack,
   Slider,
@@ -7,8 +7,9 @@ import {
   TextField,
   Typography,
   Container,
+  Alert,
 } from "@mui/material";
-import { useState } from "react";
+import React, { useState } from "react";
 
 export default function Today() {
   const [value, setValue] = useState<number>(50);
@@ -17,15 +18,31 @@ export default function Today() {
     setValue(newValue as number);
   };
 
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    window.location.reload();
+    <Alert severity="success">Your mood has been saved !</Alert>
+  }
+
   return (
     <Container sx={{ height: "75vh" }}>
       <Grid height="100%" container direction="column" spacing={2}>
         <Grid item xs />
         <Grid item>
           <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
-            <SentimentVeryDissatisfied />
-            <Slider aria-label="joy" value={value} onChange={handleChange} />
-            <Celebration />
+            <MoodBad />
+            <Slider 
+              aria-label="joy"
+              defaultValue={50}
+              value={value}
+              step={10}
+              marks={[0, 20, 50, 80, 100].map(i => ({
+                label: i,
+                value: i
+              }))}
+              valueLabelDisplay="on"
+              onChange={handleChange} />
+            <SentimentVerySatisfied />
           </Stack>
         </Grid>
         <Grid item alignItems="baseline" container spacing={2}>
@@ -34,17 +51,21 @@ export default function Today() {
           </Grid>
           <Grid item xs={12}>
             <TextField
-              rows={5}
+              rows={6}
               multiline
               fullWidth
               variant="outlined"
-              placeholder="message"
+              placeholder="explain your mood"
             />
           </Grid>
           <Grid item xs />
           <Grid item>
-            <Button variant="outlined" color="primary">
-              Log
+            <Button 
+              variant="outlined" 
+              color="primary" 
+              startIcon={< Save />} 
+              onClick={ handleClick }>
+              Save
             </Button>
           </Grid>
         </Grid>
